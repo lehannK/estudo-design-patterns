0653ffeac84d9f892f11a948b243afa689ed2d31
lehannk <leandro_hollies@hotmail.com>
2025-05-24 16:24:41 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,46 @@
+// Componente base (interface)
+interface Mensagem {
+  enviar(): string;
+}
+
+// Componente concreto
+class MensagemSimples implements Mensagem {
+  enviar(): string {
+    return "Mensagem enviada";
+  }
+}
+
+// Decorador base (classe abstrata)
+abstract class MensagemDecorator implements Mensagem {
+  constructor(protected componente: Mensagem) {}
+
+  abstract enviar(): string;
+}
+
+// Decorador concreto: adiciona criptografia
+class MensagemComCriptografia extends MensagemDecorator {
+  enviar(): string {
+    const conteudo = this.componente.enviar();
+    return `${conteudo} (com criptografia)`;
+  }
+}
+
+// Decorador concreto: adiciona log
+class MensagemComLog extends MensagemDecorator {
+  enviar(): string {
+    console.log("Log: Mensagem enviada");
+    return this.componente.enviar();
+  }
+}
+
+// Criando a mensagem básica
+let mensagem: Mensagem = new MensagemSimples();
+
+// Decorando com criptografia
+mensagem = new MensagemComCriptografia(mensagem);
+
+// Decorando com log
+mensagem = new MensagemComLog(mensagem);
+
+// Enviando a mensagem
+console.log(mensagem.enviar());
